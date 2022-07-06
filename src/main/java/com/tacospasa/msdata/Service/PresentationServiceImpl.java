43b4d2cd66547800e9bf5f60b36778e796899d5e
LaraAngel <lara.angel.js@gmail.com>
2022-07-05 23:19:18 -0500

    Presentation API created

---
+++ src/main/java/com/tacospasa/msdata/Service/PresentationServiceImpl.java
@@ -0,0 +1,37 @@
+package com.tacospasa.msdata.Service;
+
+import com.tacospasa.msdata.Entity.PresentationEntity;
+import com.tacospasa.msdata.Repository.PresentationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class PresentationServiceImpl implements PresentationService {
+    @Autowired
+    private PresentationRepository repository;
+
+    @Override
+    public PresentationEntity getPresentationById(Long id) {
+        return repository.getReferenceById(Math.toIntExact(id));
+    }
+
+    @Override
+    public List<PresentationEntity> getAllPresentations() {
+        return repository.findAll();
+    }
+
+    @Override
+    public PresentationEntity createPresentation(PresentationEntity presentation) {
+        return repository.saveAndFlush(presentation);
+    }
+
+    @Override
+    public PresentationEntity updatePresentation(PresentationEntity presentation) {
+        Long id = presentation.getId();
+        if (getPresentationById(id) != null) {
+            return repository.saveAndFlush(presentation);
+        }
+        return new PresentationEntity();
+    }
+}
