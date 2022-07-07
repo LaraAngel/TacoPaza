7781ff1165a4a2af7f3edb6713c12b320c102626
LaraAngel <lara.angel.js@gmail.com>
2022-07-06 22:23:20 -0500

    user API correction

--- src/main/java/com/tacospasa/msdata/Controllers/UserController.java
+++ src/main/java/com/tacospasa/msdata/Controllers/UserController.java
@@ -14,46 +14,39 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    @ApiOperation(value = "Get User by ID", response= UserEntity.class, tags = "Users")
+    @GetMapping(value="/user/{id}")
+    public UserEntity getUserById(@PathVariable String id){
+        return userService.getUserById(id);
+    }
+    @ApiOperation(value = "Get user by username", response= UserEntity.class, tags = "Users")
+    @GetMapping(value="/user/{userName}")
+    public UserEntity getUserByUserName(@PathVariable String userName){
+        return userService.getUserByUserName(userName);
+    }
+    @ApiOperation(value = "Get users by Status", response= UserEntity.class, tags = "Users")
+    @GetMapping(value= "/user/status")
+    public List<UserEntity> getAllUsersByStatus(@RequestParam StatusEntity status){
+        return userService.getAllUsersByStatus(status);
+    }
     @ApiOperation(value = "Get all the users", response = UserEntity.class, tags = "Users")
     @GetMapping(value = "/users")
     public List<UserEntity> getUserByRole() {
         return userService.getAllUsers();
     }
-
-    @ApiOperation(value = "Get User by ID", response= UserEntity.class, tags = "Users")
-    @GetMapping(value="/user")
-    public UserEntity getUserById(@RequestParam String id){
-        return userService.getUserById(id);
-    }
-
-    @ApiOperation(value = "Create user", response= UserEntity.class, tags = "Users")
-    @GetMapping(value="/user/{username}")
-    public UserEntity getUserByUserName(@PathVariable String userName){
-        return userService.getUserByUserName(userName);
-    }
-
     @ApiOperation(value = "Create user", response= UserEntity.class, tags = "Users")
     @PostMapping(value = "/user")
     public UserEntity createUser(@RequestBody UserEntity user){
         return userService.createUser(user);
     }
-
     @ApiOperation(value = "Update user", response= UserEntity.class, tags = "Users")
     @PutMapping(value = "/user")
     public UserEntity updateUser(@RequestBody UserEntity user){
         return userService.updateUser(user);
     }
-
     @ApiOperation(value = "Update user status", response= UserEntity.class, tags = "Users")
     @PutMapping(value = "/user/status")
     public UserEntity updateUserStatus(@RequestParam String id, @RequestParam StatusEntity status){
         return userService.updateUserStatus(id, status);
-    }
-
-    @ApiOperation(value = "Get users by Status", response= UserEntity.class, tags = "Users")
-    @GetMapping(value= "/user/status")
-    public List<UserEntity> getAllUsersByStatus(@RequestParam StatusEntity status){
-        return userService.getAllUsersByStatus(status);
     }
 }
